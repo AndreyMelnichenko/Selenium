e27741f5aacb15d81e45be870c7feab3e5a669b0
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-02-02 22:15:13 +0200

    add dependency to PageFactory project

--- src/main/java/google/Selenium/pages/GoogleSearchPage.java
+++ src/main/java/google/Selenium/pages/GoogleSearchPage.java
@@ -1,12 +1,9 @@
 package google.Selenium.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class GoogleSearchPage extends AbstractPage {
 
@@ -17,13 +14,17 @@ public class GoogleSearchPage extends AbstractPage {
     @FindBy(how= How.XPATH, using ="//input[@id='lst-ib']")
     private WebElement inputFieldLocator; // = By.xpath("//input[@id='lst-ib']");
 
-    //private WebElement searchField;
+    @FindBy(how = How.XPATH, using = "//a[@class='gb_Xf gb_Fa gb_Eb']")
+    private WebElement goGoogleEnter;
 
 
     public void searchFor (String text){
-        WebElement searchField;
-        searchField = waitFor(presenceOfElementLocated(inputFieldLocator));
+        //searchField = waitFor(presenceOfElementLocated(inputFieldLocator));
         inputFieldLocator.sendKeys(text);
         inputFieldLocator.submit();
+    }
+    public void goLoginPage(){
+        goGoogleEnter.isDisplayed();
+        goGoogleEnter.click();
     }
 }
--- src/test/java/com/google/SearchGoogleTest.java
+++ src/test/java/com/google/SearchGoogleTest.java
@@ -6,15 +6,16 @@ import google.Selenium.pages.GoogleSearchPage;
 import google.Selenium.core.WebDriverTestBase;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertTrue;
 
 public class SearchGoogleTest extends WebDriverTestBase {
 
     private final String searchData = "Selenium";
-    private final  String url = "https://www.google.com.ua";
+    private final String url = "https://www.google.com.ua";
 
     @Test
-    public void searchGoogleTest(){
+    public void searchGoogleTest() {
         driver.get(url);
         GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
         page.searchFor(searchData);
@@ -23,15 +24,11 @@ public class SearchGoogleTest extends WebDriverTestBase {
 
     }
 
-    @Test
-    public void loginPage(){
-        driver.get("https://gmail.com");
-        GoogleLoginPage loginPage = new GoogleLoginPage(driver);
-        loginPage.inputLogin();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Test (dependsOnMethods = {"searchGoogleTest"})
+    public void loginPage() {
+        driver.get(url);
+        GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
+        page.goLoginPage();
     }
 }
+
