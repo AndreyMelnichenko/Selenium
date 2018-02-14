5940a6f02e30dbe17259ab8d84697c5c40762607
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-02-14 21:44:29 +0200

    update pom

--- src/test/java/com/google/AlertTest.java
+++ src/test/java/com/google/AlertTest.java
@@ -1,14 +1,23 @@
 package com.google;
 
 import google.Selenium.core.WebDriverTestBase;
-import google.Selenium.pages.*;
+import google.Selenium.pages.AlertPage;
+import google.Selenium.pages.GoogleLoginPage;
+import google.Selenium.pages.GooglePassPage;
+import google.Selenium.pages.GoogleSearchPage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+@Feature("Advanced interactions")
+@Story("PROM-111")
 public class AlertTest extends WebDriverTestBase {
     private final String alertPage = "https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm";
     private final String dragAndDropPage = "https://www.google.com.ua";
 
+    @Description
     @Test
     public void alertTest() {
         driver.get(alertPage);
@@ -16,7 +25,6 @@ public class AlertTest extends WebDriverTestBase {
         AlertPage alertPage = PageFactory.initElements(driver, AlertPage.class);
         alertPage.pushButton();
         alertPage.alertToOk(driver);
-
     }
 
     @Test
@@ -28,9 +36,6 @@ public class AlertTest extends WebDriverTestBase {
         loginPage.inputLogin();
         GooglePassPage passPage = PageFactory.initElements(driver, GooglePassPage.class);
         passPage.inputPass(driver);
-
         searchPage.goToGoogleDrive();
-
     }
-
 }
