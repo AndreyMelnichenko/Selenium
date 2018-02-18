56dd26864e5ed00fbfd0520cc3f39db89c5b00bd
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-02-18 22:28:26 +0200

    add screen to report

--- src/main/java/google/Selenium/core/TestListener.java
+++ src/main/java/google/Selenium/core/TestListener.java
@@ -1,10 +1,6 @@
 package google.Selenium.core;
 
-import java.io.File;
-import java.io.IOException;
-
 import io.qameta.allure.Attachment;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
