4601e0918cb50c100c51450514a29ec2d62f0fc7
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-02-21 19:43:16 +0200

    complite the test

--- src/main/java/google/Selenium/core/WebDriverTestBase.java
+++ src/main/java/google/Selenium/core/WebDriverTestBase.java
@@ -3,14 +3,14 @@ package google.Selenium.core;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -28,8 +28,9 @@ public class WebDriverTestBase {
     public static final String ACCESS_KEY = "2bd030ff-f8a5-4149-9dce-0828e8bc4100";
     public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 
+    @Parameters({"platform","remoteBrowser"})
     @BeforeClass
-    protected void SetUp() throws MalformedURLException, UnexpectedException {
+    protected void SetUp(@Optional String platform, @Optional String remoteBrowser) throws MalformedURLException, UnexpectedException {
         if ((System.getProperty("user.name").toLowerCase()).equals("andrey")) {
             Configuration.browser = browser;
             switch (browser) {
@@ -40,6 +41,10 @@ public class WebDriverTestBase {
                 case FIREFOX:
                     FirefoxDriverManager.getInstance().setup();
                     driver = new FirefoxDriver();
+                    break;
+                case "remote":
+                    DesiredCapabilities caps = setDesiredCapabilities(platform, remoteBrowser);
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
                     break;
             }
         } else {
@@ -53,6 +58,15 @@ public class WebDriverTestBase {
 
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+    }
+
+    private DesiredCapabilities setDesiredCapabilities(String platform, String remoteBrowser)throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        if (platform.equalsIgnoreCase(Platform.LINUX.name())){
+            caps.setPlatform(Platform.LINUX);
+            caps.setBrowserName(remoteBrowser);
+        }
+        return caps;
     }
 
     @AfterClass
